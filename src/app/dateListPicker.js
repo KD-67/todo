f1043e8a1dd1ee4e9be4f6c3496bb25c01f40482
KD-67 <kevin.a.dominey@gmail.com>
2023-07-07 22:11:05 +0300

    fixed prev bug - date switching works

--- src/app/dateListPicker.js
+++ src/app/dateListPicker.js
@@ -1,6 +1,6 @@
 const dateListPicker = (date) => {
   const mainContent = document.querySelector(".main-content");
-  const taskListContainer = document.querySelector(".task-list-container");
+  const newTaskContainer = document.querySelector(".new-task-container");
   const mainTitle = document.querySelector(".main-title");
   const mainDate = document.querySelector(".main-date");
   const categoryTitle = document.querySelector(".category-title");
@@ -12,13 +12,13 @@ const dateListPicker = (date) => {
 
   // Create title:
   const newCategoryTitle = document.createElement("h2");
-  mainContent.insertBefore(newCategoryTitle, taskListContainer);
+  mainContent.insertBefore(newCategoryTitle, newTaskContainer);
   newCategoryTitle.classList.add("category-title");
   newCategoryTitle.innerText = "Chosen date:";
 
   // Current date:
   const newCategoryDate = document.createElement("h4");
-  mainContent.insertBefore(newCategoryDate, taskListContainer);
+  mainContent.insertBefore(newCategoryDate, newTaskContainer);
   newCategoryDate.classList.add("category-date");
   newCategoryDate.innerText = date;
 };
