bfd7e3cb2904f93e7f11baaef3da688939558f21
KD-67 <kevin.a.dominey@gmail.com>
2023-07-09 15:08:03 +0300

    working out how to use for..in loop within a for loop to display a particular category

--- src/app/utils/newTaskSubmit.js
+++ src/app/utils/newTaskSubmit.js
@@ -1,6 +1,6 @@
 import { taskFactory } from "./taskFactory";
 import { pushMasterList } from "./pushMasterList";
-
+import { loadMasterList } from "./loadMasterList";
 
 const newTaskSubmit = (name, description, due, priority, category) => {
   const taskListContainer = document.querySelector(".task-list-container");
@@ -11,57 +11,8 @@ const newTaskSubmit = (name, description, due, priority, category) => {
   // Add new task to the master list array
   pushMasterList(newTask);
 
-  // Task list (ul)
-  const taskList = document.createElement("ul");
-  taskListContainer.appendChild(taskList);
-  taskList.classList.add("task-list");
-
-  // Tasks (li)
-  const task = document.createElement("li");
-  taskList.appendChild(task);
-  task.classList.add("task");
-
-  // Task card
-  const card = document.createElement("div");
-  task.appendChild(card);
-  card.classList.add("card");
-
-  // Card info
-  // Name
-  const taskName = document.createElement("h4");
-  card.appendChild(taskName);
-  taskName.innerText = newTask.name;
-
-  // Description
-  const taskDescription = document.createElement("p");
-  card.appendChild(taskDescription);
-  taskDescription.innerText = newTask.description;
-
-  // Priority
-  const taskPriority = document.createElement("p");
-  card.appendChild(taskPriority);
-  taskPriority.innerText = "Priority: " + newTask.priority;
-
-  // Category
-  const taskCategory = document.createElement("p");
-  card.appendChild(taskCategory);
-  if (newTask.category) {
-    taskCategory.innerText = "Category: " + newTask.category;
-  }
-
-  // Completed Button
-  const completedBtn = document.createElement("button");
-  card.appendChild(completedBtn);
-  completedBtn.classList.add("completed-btn");
-  completedBtn.innerText = "Done";
-
-  // Completed button functionality
-  completedBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    taskList.removeChild(task);
-  });
-
-  return newTask;
+  //test
+  loadMasterList();
 };
 
 export { newTaskSubmit };
