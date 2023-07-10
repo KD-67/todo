bb48b03d67b7e8feb4e44b0cd46642c2b8c2b896
KD-67 <kevin.a.dominey@gmail.com>
2023-07-10 20:13:21 +0300

    working task display upon initial creation

---
+++ src/app/utils/initialTaskLoad.js
@@ -0,0 +1,59 @@
+import { masterListArray } from "./masterListArray";
+
+const initialTaskLoad = (name, description, priority, category) => {
+  const taskListContainer = document.querySelector(".task-list-container");
+
+  // Task list (ul)
+  const taskList = document.createElement("ul");
+  taskListContainer.appendChild(taskList);
+  taskList.classList.add("task-list");
+
+  // Tasks (li)
+  const task = document.createElement("li");
+  taskList.appendChild(task);
+  task.classList.add("task");
+
+  // Task card
+  const card = document.createElement("div");
+  task.appendChild(card);
+  card.classList.add("card");
+
+  // Card info
+  // Name
+  const taskName = document.createElement("h4");
+  card.appendChild(taskName);
+  taskName.innerText = name;
+
+  // Description
+  const taskDescription = document.createElement("p");
+  card.appendChild(taskDescription);
+  taskDescription.innerText = description;
+
+  // Priority
+  const taskPriority = document.createElement("p");
+  card.appendChild(taskPriority);
+  taskPriority.innerText = "Priority: " + priority;
+
+  // Category
+  const taskCategory = document.createElement("p");
+  card.appendChild(taskCategory);
+  if (category) {
+    taskCategory.innerText = "Category: " + category;
+  }
+
+  // Completed Button
+  const completedBtn = document.createElement("button");
+  card.appendChild(completedBtn);
+  completedBtn.classList.add("completed-btn");
+  completedBtn.innerText = "Done";
+
+  // Completed button functionality
+  completedBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    taskList.removeChild(task);
+    let taskIndex = masterListArray.indexOf(task);
+    masterListArray.splice(taskIndex, 1);
+  });
+};
+
+export { initialTaskLoad };
--- src/app/utils/newTaskSubmit.js
+++ src/app/utils/newTaskSubmit.js
@@ -1,5 +1,6 @@
 import { taskFactory } from "./taskFactory";
 import { pushMasterList } from "./pushMasterList";
+import { initialTaskLoad } from "./initialTaskLoad";
 
 const newTaskSubmit = (name, description, due, priority, category) => {
   // create new task object
@@ -7,6 +8,11 @@ const newTaskSubmit = (name, description, due, priority, category) => {
 
   // Add new task to the master list array
   pushMasterList(newTask);
+
+  // Initial task load to DOM (if fits in active category)
+  if (1 === 1) {
+    initialTaskLoad(name, description, due, priority, category);
+  }
 };
 
 export { newTaskSubmit };
