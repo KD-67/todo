f87fd36f620c9e915594f972854038d4b36ce4c9
KD-67 <kevin.a.dominey@gmail.com>
2023-07-11 08:51:49 +0300

    initial task display for active date category and active master list category

--- src/app/utils/newTaskSubmit.js
+++ src/app/utils/newTaskSubmit.js
@@ -4,6 +4,9 @@ import { initialTaskLoad } from "./initialTaskLoad";
 
 const newTaskSubmit = (name, description, due, priority, category) => {
   const categoryDisplay = document.querySelector(".category-display");
+  const mainDate = document.querySelector(".main-date");
+  const mainTitle = document.querySelector(".main-title");
+  const categoryDate = document.querySelector(".category-date");
 
   // create new task object
   const newTask = taskFactory(name, description, due, priority, category);
@@ -13,6 +16,10 @@ const newTaskSubmit = (name, description, due, priority, category) => {
 
   // Initial task load to DOM (if fits in active category)
   if (categoryDisplay && categoryDisplay.innerText === category) {
+    initialTaskLoad(name, description, priority, category);
+  } else if (categoryDate && categoryDate.innerText === due) {
+    initialTaskLoad(name, description, priority, category);
+  } else if (mainTitle && mainTitle.innerText == "Master List") {
     initialTaskLoad(name, description, priority, category);
   }
 };
